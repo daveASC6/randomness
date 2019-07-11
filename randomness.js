7902200b489b54a3f11cf314af6d8a6d425cb94a
daveASC6 <daveurlajide004@gmail.com>
2019-07-11 11:57:42 -0400

    tried to draw random numbers foods and names, wasnt done

---
+++ randomness.js
@@ -0,0 +1,26 @@
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+"Bob",
+"Martin",
+"Danny",
+"Madhi",
+"Harry",
+"Sean",
+"Robert"
+];
+
+function randomNumber() {
+    const startingnumber = Math.random()
+    console.log(startingnumber * 21);
+}
+
+randomNumber();
